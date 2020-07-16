9c18eda57eabf713ec6fe4f6a2dde7993ceb11ed
leecr1215 <leecr1215@naver.com>
2020-07-16 20:23:38 +0900

    home activity

--- app/src/main/java/com/example/bottomup2020/HomeActivity.java
+++ app/src/main/java/com/example/bottomup2020/HomeActivity.java
@@ -2,7 +2,11 @@ package com.example.bottomup2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,48 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home2);
+
     }
+
+    // R.menu.home_menu라는 코드로 res/menu/home_menu.xml에 있는 코드 읽어옴
+    // 메뉴버튼을 눌렀을 때 보여줄 menu에 대해 정의
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    // 메뉴의 항목을 클릭했을 때 호출되는 콜백 메서드
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            // id가 subMenu_C인 메뉴를 누르면 SolutionActivity_c로 이동함.
+            case R.id.subMenu_C:
+                Intent intent = new Intent(this, SolutionActivity_c.class);
+                startActivity(intent);
+                break;
+            case R.id.subMenu_JAVA:
+                intent = new Intent(this, SolutionActivity_java.class);
+                startActivity(intent);
+                break;
+            case R.id.subMenu_PYTHON:
+                intent = new Intent(this, SolutionActivity_python.class);
+                startActivity(intent);
+                break;
+            case R.id.FavoritesMenu:
+                intent = new Intent(this, FavouritesActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
+    }
+
+    // 메뉴가 화면에 보여질 때 마다 호출됨.
+    // 메뉴를 눌렀을 때 이미 적용되어있어야 하는 정보들(ex) 로그인안하면 로그아웃 버튼 없게)
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+       return super.onPrepareOptionsMenu(menu);
+    }
+
+
 }
