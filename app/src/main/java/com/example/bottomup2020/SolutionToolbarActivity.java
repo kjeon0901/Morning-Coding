cdc3674241b5b1f702e9eb19aa5b0171f2411f15
leecr1215 <leecr1215@naver.com>
2020-07-19 02:33:34 +0900

    so many!

--- app/src/main/java/com/example/bottomup2020/SolutionToolbarActivity.java
+++ app/src/main/java/com/example/bottomup2020/SolutionToolbarActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
@@ -74,5 +75,6 @@ public class SolutionToolbarActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
 
 }
