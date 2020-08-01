1f5dcd6d34693c9056e44a1133c645577a214434
leecr1215 <leecr1215@naver.com>
2020-08-01 17:39:21 +0900

    ra

--- app/src/main/java/com/example/bottomup2020/FavouritesActivity.java
+++ app/src/main/java/com/example/bottomup2020/FavouritesActivity.java
@@ -1,8 +1,15 @@
 package com.example.bottomup2020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.example.bottomup2020.R;
 
@@ -13,4 +20,68 @@ public class FavouritesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourites);
     }
+
+
+    @Override
+    public void setContentView(int layoutResID){
+        LinearLayout fullView = (LinearLayout)getLayoutInflater().inflate(R.layout.activity_home_toolbar, null);
+        FrameLayout activityContainer = (FrameLayout)fullView.findViewById(R.id.activity_content);
+        getLayoutInflater().inflate(layoutResID, activityContainer, true);
+        super.setContentView(fullView);
+
+        Toolbar toolbar=(Toolbar)findViewById(R.id.c_toolbar);
+        //툴바 사용여부 결정(기본=사용)
+        if(useToolbar()){
+            setSupportActionBar(toolbar);
+            setTitle("Java 01번");
+        }else{
+            toolbar.setVisibility(View.GONE);
+        }
+    }
+
+    //툴바 사용할지 말지 정함
+    protected boolean useToolbar(){
+        return true;
+    }
+
+    // R.menu.home_menu라는 코드로 res/menu/home_menu.xml에 있는 코드 읽어옴
+    // 메뉴버튼을 눌렀을 때 보여줄 menu에 대해 정의
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    // 메뉴의 항목을 클릭했을 때 호출되는 콜백 메서드
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            // id가 subMenu_C인 메뉴를 누르면 SolutionActivity_c로 이동함.
+            case R.id.subMenu_C:
+                Intent intent = new Intent(this, SolutionActivity_c.class);
+                startActivity(intent);
+                break;
+            case R.id.subMenu_JAVA:
+                intent = new Intent(this, SolutionActivity_java.class);
+                startActivity(intent);
+                break;
+            case R.id.subMenu_PYTHON:
+                intent = new Intent(this, SolutionActivity_python.class);
+                startActivity(intent);
+                break;
+            case R.id.FavoritesMenu:
+                intent = new Intent(this, FavouritesListActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
+    }
+
+    // 메뉴가 화면에 보여질 때 마다 호출됨.
+    // 메뉴를 눌렀을 때 이미 적용되어있어야 하는 정보들(ex) 로그인안하면 로그아웃 버튼 없게)
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
 }
